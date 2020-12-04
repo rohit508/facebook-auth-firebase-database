d93d64f4283d7811b01701a208f4feccd21bf9d1
rohit508 <lachmanrohit282@gmail.com>
2020-12-04 16:02:39 +0500

    facebook-auth

---
+++ app.js
@@ -0,0 +1,12 @@
+const facebookauth = () =>{
+  var provider = new firebase.auth.FacebookAuthProvider();
+  firebase.auth().signInWithPopup(provider)
+  .then(function(result) {
+    var user = result.user;
+    console.log("user",user)
+  }).catch(function(error) {
+  console.log(error)
+    
+  });
+}
+
